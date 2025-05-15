f0deadd1b70606da416f89dbe12ad5f2e20f3548
Renan Assis <renan.assis@sounuv.com>
2025-05-15 20:05:03 -0300

    Ajuste fundo

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -2,10 +2,12 @@
 
 import { HeroSection } from "@/components/home/sections/hero-section";
 import { FooterSection } from "@/components/home/sections/footer-section";
+import { ScatteredParticles } from "@/components/ui/scattered-particles";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen w-full">
+    <main className="flex flex-col items-center justify-center min-h-screen w-full bg-black">
+      <ScatteredParticles />
       <div className="w-full">
         <HeroSection />
         <FooterSection />
